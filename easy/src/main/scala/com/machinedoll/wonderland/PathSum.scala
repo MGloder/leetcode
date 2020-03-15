d9811ddf7ebc09e192a8e5dc6778fe56499aaf3d
RiderOfE <yanxuok@gmail.com>
2020-03-15 22:26:20 +0800

    refactor a bit

--- easy/src/main/scala/com/machinedoll/wonderland/PathSum.scala
+++ easy/src/main/scala/com/machinedoll/wonderland/PathSum.scala
@@ -3,28 +3,13 @@ package com.machinedoll.wonderland
 object PathSum {
   def hasPathSum(root: TreeNode, sum: Int): Boolean = {
     def checkPathSum(node: TreeNode, current: Int): Boolean = {
-      if (node == null) {
-        false
-      } else {
-        if (node.left == null && node.right == null) {
-          if (current + node.value == sum)
-            return true
-          false
-        } else {
-          var leftResult = false
-          var rightResult = false
-          if (node.left != null) {
-            leftResult = checkPathSum(node.left, current + node.value)
-          }
-          if (node.right != null) {
-            rightResult = checkPathSum(node.right, current + node.value)
-          }
-          if (leftResult || rightResult)
-            return true
-          false
-        }
-      }
+      if (node == null)
+        return false
+      if (node.left == null && node.right == null && current + node.value == sum)
+        return true
+      return checkPathSum(node.left, current + node.value) || checkPathSum(node.right, current + node.value)
     }
+
     return checkPathSum(root, 0)
+
   }
-}

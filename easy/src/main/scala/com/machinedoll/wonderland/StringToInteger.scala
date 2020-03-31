a75dfd3e169a979e2d121bed70a55fd2ed44ca25
Eric Yan <yanxuok@gmail.com>
2020-03-31 10:02:50 +0800

    added atoi

--- easy/src/main/scala/com/machinedoll/wonderland/StringToInteger.scala
+++ easy/src/main/scala/com/machinedoll/wonderland/StringToInteger.scala
@@ -2,6 +2,27 @@ package com.machinedoll.wonderland
 
 class StringToInteger {
   def myAtoi(str: String): Int = {
-    ???
+    val findValidStr = "^\\s*[-+]?[0-9]+.*".r
+    val findNumbers = "^\\s*[-+]?[0-9]+".r
+    var result: Long = 0
+    str match {
+      case findValidStr() => {
+        val intStr = findNumbers.findAllIn(str).mkString
+        var flag = 1
+        for (i <- 0 until intStr.length) {
+          val item = intStr.charAt(i)
+          if (item == '-' || item == '+' || item == ' ') {
+            if (item == '-') flag = -1
+          } else {
+            result = result * 10 + item.asDigit
+            if (result * flag > Int.MaxValue) return Int.MaxValue
+            else if (result * flag < Int.MinValue) return Int.MinValue
+          }
+        }
+        result *= flag
+        result.toInt
+      }
+      case _ => 0
+    }
   }
 }

5232617334451fd434fa8bac7c8737c040617d29
Eric Yan <yanxuok@gmail.com>
2020-04-01 10:01:01 +0800

    added klargest

--- easy/src/main/scala/com/machinedoll/wonderland/KthLargestElementInStream.scala
+++ easy/src/main/scala/com/machinedoll/wonderland/KthLargestElementInStream.scala
@@ -1,8 +1,16 @@
 package com.machinedoll.wonderland
 
+import scala.collection.mutable
+
+
 class KthLargestElementInStream(_k: Int, _nums: Array[Int]) {
-  //TODO
+
+  private val minHeap = mutable.PriorityQueue[Int](_nums: _*)(Ordering.by(e => -e))
+
   def add(`val`: Int): Int = {
-    ???
+    minHeap.enqueue(`val`)
+    while (minHeap.size > _k) minHeap.dequeue()
+
+    minHeap.head
   }
 }

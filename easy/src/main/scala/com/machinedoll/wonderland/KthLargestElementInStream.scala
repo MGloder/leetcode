0a43bb5b0636acb571e4e479a1ffa4e23b2f85e5
Eric Yan <yanxuok@gmail.com>
2020-04-01 09:23:09 +0800

    added kth largest elment

---
+++ easy/src/main/scala/com/machinedoll/wonderland/KthLargestElementInStream.scala
@@ -0,0 +1,8 @@
+package com.machinedoll.wonderland
+
+class KthLargestElementInStream(_k: Int, _nums: Array[Int]) {
+  //TODO
+  def add(`val`: Int): Int = {
+    ???
+  }
+}
---
+++ easy/src/test/scala/com/machinedoll/wonderland/KthLargestElementInStreamTest.scala
@@ -0,0 +1,9 @@
+package com.machinedoll.wonderland
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class KthLargestElementInStreamTest extends AnyFunSuite {
+  test("[4,5,8,2], 3 is 4") {
+
+  }
+}
